ca9f7aa4b383dd5fcee9e849349b02820670a721
Jackobjack <jackobjack@yahoo.de>
2023-10-25 22:51:15 +0800

    Finance module ruu item add hiideg bolloo

--- app.js
+++ app.js
@@ -1,96 +1,130 @@
 // Дэлгэцийн модуль: UI - User Interface
 var uiController = (function(){
-    // далдалсан хэсэг: дотор хэсэг = data encapsulation    
-    var DOMStrings = {
-        inputType : '.add__type',
-        inputDescription : '.add__description',
-        inputValue : '.add__value',
-        addBtn : '.add__btn'   
-    }
+  var DOMstrings = {
+    inputType : '.add__type',
+    inputDescription : '.add__description',
+    inputValue : '.add__value',
+    addBtn : '.add__btn'
+  }
 
-    return {
-        getInput : function(){
-            return {
-                type : document.querySelector(DOMStrings.inputType).value,
-                description: document.querySelector(DOMStrings.inputDescription).value,
-                value: document.querySelector(DOMStrings.inputValue).value
-            }
-        }, 
-        getDOMStrings : function(){
-            return DOMStrings;
+  return {
+      getInput : function(){
+        return {
+          type : document.querySelector(DOMstrings.inputType).value,
+          description : document.querySelector(DOMstrings.inputDescription).value,
+          value : document.querySelector(DOMstrings.inputValue).value,
         }
-    }
-
+      },
+      getDOMstrings : function(){
+        return DOMstrings
+      }
+  }
+ 
 })();
 
 // Санхүүгийн модуль: Finance
 var financeController = (function () {
-    // Байгуулагч функц 
-    var Income = function(id, description, value){
-        this.id = id
-        this.description = description
-        this.value = value
-    }
+  // Байгуулагч функц 
+  var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value
+  }
+  
+  var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value
+  }
 
-    var Expense = function(id, description, value){
-        this.id = id
-        this.description = description
-        this.value = value
+  // private data
+  var data = {
+    items : {
+      inc : [],
+      exp : []
+    },
+    totals : {
+      inc : 0,
+      exp : 0
     }
+  }
 
-    var Data = {
-        allItems : {
-            inc : [],
-            exp : []
-        },
-        totals: {
-            inc: 0,
-            exp: 0
-        }
+ 
+  return {
+    addItem : function(type, desc, val){
+      var id, item;
+
+      if (data.items[type].length === 0) {
+        id = 1
+      } else {
+        // хамгийн сүүлийн элементийн id-г олоод 1-р нэмэгдүүлсэн
+        id = data.items[type][data.items[type].length - 1].id + 1
+      }
+
+      // ID = Identification
+      if (type === 'inc'){
+        item = new Income(id, desc, val)
+      } else {
+        item = new Expense(id, desc, val)
+      }
+
+      data.items[type].push(item)
+      data.totals[type] = data.totals[type] + val      
+    }, 
+    seeData : function(){
+      return data;
     }
+  }
 
-})();
+  }
+
+)();
 
 // Холбогч модуль: App
 var appController = (function(uiController , financeController){
 
-    var ctrlAddItem = function (){
-        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна
-        console.log(uiController.getInput());
-    
-        // 2. Олж авсан өгөгдлийг санхүүгийн контролерт дамжуулж тэнд хадгална.
-    
-        // 3. Олж авсан өгөгдлүүдээ вэбийн тохирох хэсэгт нь гаргана
-    
-        // 4. Төсвийг тооцоолно.
-    
-        // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-    }
+  var ctrlAddItem = function(){
+    // 1. Оруулах өгөгдлийг Браузераас олж авна
+    var input = uiController.getInput()
 
-    var setupEventListener = function(){
-        // DOM === DOMStrings
-        var DOM = uiController.getDOMStrings();
+    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална
+    financeController.addItem(input.type, input.description, input.value)
+    // console.log(data.);
 
-        document.querySelector(DOM.addBtn).addEventListener('click', function(){
-            ctrlAddItem()
-        });
-    
-        document.addEventListener('keypress', function (event) {
-            if (event.keyCode === 13 || event.which === 13) {
-                ctrlAddItem()
-            }
-        })
+    // 3. Боловсруулсан утгыг вэб дээр тохирох хэсэгт гаргана
+
+    // 4. Төсвийг тооцоолно
+
+    // 5. Эцсийн үлдэгдэл, тооцоог браузер дээр харуулна
+
+  }
+
+  var setupEventListeners = function(){
+    // addButton === '.add__btn'  
+    var DOM = uiController.getDOMstrings();
+    document.querySelector(DOM.addBtn).addEventListener('click', function(){
+      ctrlAddItem()
+    })
+
+    document.addEventListener('keypress', function(event){
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem()
+    } 
+    })
+  }
+
+  return {
+    init : function(){
+      setupEventListeners()
+      console.log('программ эхэллээ : '); 
     }
-    
-    return {
-        init : function(){
-            console.log('Application started :>> ');
-            setupEventListener();
-        }
-    }
+  }
+
 })(uiController, financeController);
 
-appController.init()
+
+appController.init();
+
 
 
 
